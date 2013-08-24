0bd626fb6c05e336613bceaa19c56ce829f03af1
Ben Simonsen <bsimonse@vt.edu>
2013-08-23 22:12:11 -0400

    I hate Nikki Men... Min... Manaj?

--- TeleTweet/src/main/java/com/bacon/teletweet/HomeActivity.java
+++ TeleTweet/src/main/java/com/bacon/teletweet/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.bacon.teletweet;
 
-import android.os.Bundle;
+import twitter4j.*;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import java.util.List;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class HomeActivity extends Activity {
 
@@ -12,7 +17,40 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		
+		//setup config
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true);
+		cb.setOAuthConsumerKey("").setOAuthConsumerSecret("")
+		.setOAuthAccessToken("").setOAuthAccessTokenSecret("");
+		
+		//setup listeners
+		TwitterListener li = new TwitterAdapter()
+		{
+			@Override public void searched(QueryResult result)
+			{
+				List<Status> twits = result.getTweets();
+				Log.i("TeleTweet","Got "+twits.size()+" tweets");
+				Log.i("TeleTweet","Tweet 1 is "+twits.get(0).getText());
+			}
+			
+			@Override public void onException(TwitterException e, int method)
+			{
+				Log.e("TeleTweet", "Crap, the "+method+" method broke. "+e.getErrorMessage());
+			}
+		};
+		
+		//make tweets!
+		AsyncTwitter twit = new AsyncTwitterFactory(cb.build()).getInstance();
+		twit.addListener(li);
+		twit.search(new Query("foo"));
+		Log.i("TeleTweet","Welp, here goes nothing!");
+	}
+	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
