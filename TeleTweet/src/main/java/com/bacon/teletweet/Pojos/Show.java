2c4b08798b10f6973913d1372c02664c0cafa257
Droidweb <triplemwebdesigns@gmail.com>
2013-08-24 00:13:12 -0400

    Added a POJO and a JSON feed.  I am so doing work foo'!

---
+++ TeleTweet/src/main/java/com/bacon/teletweet/Pojos/Show.java
@@ -0,0 +1,12 @@
+package com.bacon.teletweet.Pojos;
+
+/**
+ * Created by Droidweb on 8/23/13.
+ */
+public class Show {
+    private String name;
+    private String description;
+    private String imageURL;
+
+    // will add more as needed
+}
