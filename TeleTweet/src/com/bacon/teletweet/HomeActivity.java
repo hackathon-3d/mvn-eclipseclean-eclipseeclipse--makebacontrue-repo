8a525d2f88497fb7022940e4daeabbd4ac11fd4c
Ben Simonsen <bsimonse@vt.edu>
2013-08-24 02:11:14 -0400

    This is actually getting cool.

--- TeleTweet/src/com/bacon/teletweet/HomeActivity.java
+++ TeleTweet/src/com/bacon/teletweet/HomeActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import com.bacon.teletweet.Utility.TwitterUtil;
 
 public class HomeActivity extends Activity {
@@ -13,12 +15,25 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_splash);
 		
-		TwitterUtil.initAndAwaitAuthenticationResult(this);
+		Button b = (Button)findViewById(R.id.loginButton);
+		b.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v)
+			{
+				login();
+			}
+		});
 	}
 	
 	@Override public void onActivityResult(int request, int result, Intent i)
 	{
 		TwitterUtil.OauthComplete(i);
+	}
+	
+	private void login()
+	{
+
+		TwitterUtil.initAndAwaitAuthenticationResult(this);
 	}
 	
     @Override
